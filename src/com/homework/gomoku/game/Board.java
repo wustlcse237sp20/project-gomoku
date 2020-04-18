614d4d4295860567e179bf971c054ecfb343ca42
woodcutter1998 <jeric05908@gmail.com>
2020-04-18 17:07:15 -0500

    clean up structure.

--- src/com/homework/gomoku/game/Board.java
+++ src/com/homework/gomoku/game/Board.java
@@ -4,5 +4,4 @@ public interface Board {
     public Move getPieceAt(int row, int col);
     public Boolean placeMove(Move move);
     public int getBoardSize();
-    public Move[][] getBoardGrid();
 }
