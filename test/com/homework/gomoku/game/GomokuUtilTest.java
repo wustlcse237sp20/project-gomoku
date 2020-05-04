443fe3a2fad6fe00716a0e11899f2d5c4cb875cb
woodcutter1998 <jeric05908@gmail.com>
2020-05-04 01:35:09 -0500

    include AI, and fixing bugs, extract GomokuUtil

---
+++ test/com/homework/gomoku/game/GomokuUtilTest.java
@@ -0,0 +1,20 @@
+package com.homework.gomoku.game;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GomokuUtilTest {
+
+    @Test
+    public void isInBoard() {
+    }
+
+    @Test
+    public void isPieceOnDir() {
+    }
+
+    @Test
+    public void gridRound() {
+    }
+}
